1a243ee383c91f8bfff0dc5e5ce8b34f01300cbb
Dennis Pham <dennispham@tutanota.com>
2021-10-22 17:11:13 -0500

    create initial files and environment for backend.

---
+++ backend/main.py
@@ -0,0 +1,33 @@
+# Standard Library modules
+from datetime import date
+
+# Third party libraries
+from flask import Flask
+from flask_restful import Resource, Api
+
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+
+# globals
+app = Flask(__name__)
+api = Api(app)
+
+temp_data = {}
+
+
+class CheckIn(Resource): #TODO make the actual endpoint
+    """Check in Resource class"""
+    def get(self, employee_id):
+        return temp_data[employee_id]
+
+    def put(self, employee_id):
+        temp_data[employee_id] = date.today()
+
+
+# Add resources here
+api.add_resource(CheckIn, '/<string:employee_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
